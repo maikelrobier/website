b8974f48f54221f430696c02b2395bd6a0cfae0b
maikelrobier <maikelrobier@gmail.com>
2017-12-25 17:50:55 -0600

    Make server pring website network address

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+var os = require('os')
 const path = require('path')
 const express = require('express')
+var _ = require('lodash')
+
+var networkIP = _.chain(os.networkInterfaces())
+  .values()
+  .flatten()
+  .find({ family: 'IPv4', internal: false })
+  .get('address')
+  .value() || '127.0.0.1'
+
+const port = process.env.PORT
 
 const app = express()
 
@@ -9,4 +20,11 @@ app.get('*', function(request, response, next) {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT)
+
+app.listen(port)
+
+console.log(`
+  App running:
+  - Local address: http://localhost:${port}
+  - Network address: http://${networkIP}:${port}
+`)
