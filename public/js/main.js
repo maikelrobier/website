69849586f636f586953201f2499f8a34fc4c7bc6
maikelrobier <maikelrobier@gmail.com>
2017-12-25 19:11:11 -0600

    Make facebook link open app in iphone

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+var FACEBOOK_PROFILE_ID = "100008985827039"
+const FACEBOOK_URL_NAME = "maikelrobier"
+
+function gotoFacebookProfile() {
+  // Need this workaround since facebook uses App Links instead of Deep Links
+  if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+    // no need to open a new tab
+    window.location = "fb://profile/" + FACEBOOK_PROFILE_ID
+  } else {
+    // open in a new tab
+    window.open("https://facebook.com/" + FACEBOOK_URL_NAME)
+  }
+}
+
+function handleFacebookLinkClick(e) {
+  e.preventDefault()
+  e.stopPropagation()
+  gotoFacebookProfile()
+  return false
+}
+
+var fbLink = document.querySelector('#facebook-link')
+fbLink.onclick = handleFacebookLinkClick
